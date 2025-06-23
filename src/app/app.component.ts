5e469c0da44a94ddac5dad17b4f6309c2a1be60e
zakariya <zakariya_chergui@ens.univ-artois.fr>
2025-06-23 16:59:47 +0200

    ➕ Add tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TestNgPrimeComponent} from './components/test-ng-prime/test-ng-prime.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestNgPrimeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/test-ng-prime/test-ng-prime.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-test-ng-prime',
+  imports: [
+    Button
+  ],
+  templateUrl: './test-ng-prime.component.html',
+  styleUrl: './test-ng-prime.component.scss'
+})
+export class TestNgPrimeComponent {
+
+}
