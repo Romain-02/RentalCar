3a4622c97441186014b19c04c5314dc9362a7a3e
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2025-06-25 10:44:25 +0200

    :sparkles: Add the list component for cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CarListComponent} from './components/car-list/car-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
