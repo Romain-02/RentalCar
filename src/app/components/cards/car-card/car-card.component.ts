fb3f09de33f1f037531bd9a9843bdc09db935abc
zakariya <zakariya_chergui@ens.univ-artois.fr>
2025-06-25 16:06:05 +0200

    ✨ Add reservation button, closes #24

--- src/app/components/cards/car-card/car-card.component.ts
+++ src/app/components/cards/car-card/car-card.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Car } from '../../../models/Car';
+import {RouterLink} from "@angular/router";
 
 // ==============================================
 
 
 @Component({
   selector: 'app-car-card',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './car-card.component.html',
   styleUrl: './car-card.component.scss'
 })
