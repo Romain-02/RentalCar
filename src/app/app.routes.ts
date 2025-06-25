af39694664a8ed05523d14452c407cad0010440a
zakariya <zakariya_chergui@ens.univ-artois.fr>
2025-06-25 10:36:07 +0200

    ✨ Add defaultLayout & homePage & road

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {DefaultLayoutComponent} from './components/default-layout/default-layout.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        title: 'Accueil',
+        component: HomePageComponent
+      }
+    ]
+  }
+];
