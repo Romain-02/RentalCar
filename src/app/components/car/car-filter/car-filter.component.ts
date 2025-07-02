ea4d40eff4fff1238e9ea03568ca08b2f690a698
romain_notteau <romain_notteau@ens.univ-artois.fr>
2025-07-02 15:45:00 +0200

    correction date filtre #81

--- src/app/components/car/car-filter/car-filter.component.ts
+++ src/app/components/car/car-filter/car-filter.component.ts
@@ -94,7 +94,8 @@ export class CarFilterComponent implements OnInit{
   }
 
   private formatDate(date: Date): string {
-    return date.toISOString().split('T')[0];
+    const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+    return localDate.toISOString().split('T')[0];
   }
 
   public resetFilters(): void {
